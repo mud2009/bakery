a0596de36ef6c11f8d2921e5f82f61d13a138798
Will Greenberg <greenberg.will@gmail.com>
2022-02-25 10:50:51 -0800

    add second test for bread constructor, in order to add amount to bread object

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -12,5 +12,12 @@ namespace Bakery.Tests
       Bread breadOne = new Bread();
       Assert.AreEqual(typeof(Bread), breadOne.GetType());
     }
+    [TestMethod]
+    public void BreadConstructor_GetsAmount_Int()
+    {
+      int num = 4;
+      Bread breadOne = new Bread(num);
+      Assert.AreEqual(num, breadOne.Amount);
+    }
   }
 }
