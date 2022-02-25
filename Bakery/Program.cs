8b4885968146a0af0b7da905e825900aa8aa0afa
Will Greenberg <greenberg.will@gmail.com>
2022-02-25 08:55:33 -0800

    add command line interface to main

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Bakery.Models;
 
 namespace Bakery
 {
@@ -7,6 +8,13 @@ namespace Bakery
   {
     public static void Main()
     {
+      System.Console.WriteLine("Welcome to the bakery");
+      System.Console.WriteLine("One loaf of bread is 5 dollars");
+      System.Console.WriteLine("One pastry is 2 dollars");
+      System.Console.WriteLine("How many loaves of bread would you like?");
+      int userLoaves = Console.ReadLine();
+      System.Console.WriteLine("How many pastries would you like?");
+      int userPastries = Console.ReadLine();
     }
   }
 }
