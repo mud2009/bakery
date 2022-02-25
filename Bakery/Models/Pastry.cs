103818725aec80da00e50b9eed79270ad482c5ee
Will Greenberg <greenberg.will@gmail.com>
2022-02-25 13:28:56 -0800

    add set to pastry amount property

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -2,11 +2,16 @@ namespace Bakery.Models
 {
   public class Pastry
   {
-    public int Amount{ get; }
+    public int Amount{ get; set; }
 
     public Pastry(int amount)
     {
       Amount = amount;
     }
+
+    // public int GetPrice()
+    // {
+
+    // }
   }
 }
