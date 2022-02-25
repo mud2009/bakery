9fa07b5c11c46b8c4ad001f27d6ff0b82ecc5820
Will Greenberg <greenberg.will@gmail.com>
2022-02-25 10:15:18 -0800

    add validation for int in program interface

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -12,9 +12,31 @@ namespace Bakery
       System.Console.WriteLine("One loaf of bread is $5");
       System.Console.WriteLine("One pastry is $2");
       System.Console.WriteLine("How many loaves of bread would you like?");
-      int userLoaves = int.Parse(Console.ReadLine());
+      string stringLoaves = Console.ReadLine();
+      int loaves;
+      bool loavesSuccess = int.TryParse(stringLoaves, out loaves);
+      if (loavesSuccess)
+      {
+        // whatever the code should do
+      }
+      else
+      {
+        System.Console.WriteLine("---Please enter a number---");
+        Main();
+      }
       System.Console.WriteLine("How many pastries would you like?");
-      int userPastries = int.Parse(Console.ReadLine());
+      string stringPastries = Console.ReadLine();
+      int pastries;
+      bool pastriesSuccess = int.TryParse(stringPastries, out pastries);
+      if (pastriesSuccess)
+      {
+        // whatever the code should do
+      }
+      else
+      {
+        System.Console.WriteLine("---Please enter a number---");
+        Main();
+      }
     }
   }
 }
